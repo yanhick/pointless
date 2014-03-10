39d8cd83441d2c5062e1f778f05c35309fab7b40
Yannick <yannick.dominguez@gmail.com>
2014-03-10 16:24:19 +0100

    working on front-end

--- pointless.js
+++ pointless.js
@@ -1,6 +1,8 @@
 var testData = "{";
+    testData = '"template" : {';
     testData += '"html" : "<div class=\'slide\'>text<img src=\'image\' /></div>",';
     testData += '"css" : ".slide \{ background:black; color:white;\}",';
+    testData += "},";
     testData += '"slides" : [';
     testData += "{"
     testData += '"text" : "test text",';
@@ -13,29 +15,21 @@ var testData = "{";
     testData += "]";
     testData += "}";
 
-var presentation = {};
 
-var slidesIndex = 0;
-var pagination = {
-    index : 0
-}
 
 // STARTUP
 
 window.onload = function () {
 
     //TODO : fetch
-    presentation = JSON.parse(testData);
+    presentation.load();
 
-    listenKeyboard(undefined, next.bind(presentation),
-                   undefined, previous.bind(presentation));
+    //TODO : listen for show/hide UI
+    listenKeyboard(undefined, pagination.next,
+                   undefined, pagination.previous);
 
-    listenContentChange(updateContent.bind(presentation));
-    listenTemplateChange(updateTemplate(presentation));
-
-        //listen for next/prev
-        //listen for show/hide UI
-    updatePresentation(presentation);
+    listenContentChange(presentation.updateContent);
+    listenTemplateChange(presentation.updateTemplate);
 }
 
 // USER INTERACTIONS
@@ -90,39 +84,41 @@ function listenTemplateChange(updateTemplate) {
     });
 }
 
-function updatePresentation(presentation, getIndex) {
-    changeSlide(presentation, getIndex);
-    updateStyle(presentation.css);
-    updateForms(presentation, getIndex);
-}
 
-function updateForms(presentation, getIndex) {
-    document.querySelector(".content textarea").value = presentation.slides[getIndex()].text;
-    document.querySelector(".content [type=url]").value = presentation.slides[getIndex()].image;
-    document.querySelector(".template .html").value = presentation.html;
-    document.querySelector(".template .css").value = presentation.css;
-}
+// PRESENTATION
 
-function next(presentation, getIndex, incrIndex) {
+var presentation = (function(onContentChange, onTemplateChange, getIndex) {
 
-    var previousIndex = getIndex();
-    return changeSlide(presentation, incrIndex(), previousIndex );
-}
+    var data = {},
 
-function previous(presentation, getIndex, decrIndex) {
-    var previousIndex = getIndex();
+        updateSlide = function (getIndex, slide) {
+            data.slides[getIndex()] = slide;
+            onContentChange(data[getIndex()], data.template);
+        },
 
-    if (previousIndex === 0)
-        return;
+        updateTemplate = function(template) {
+            data.template = template;
+            onTemplateChange(date[getIndex()], data.template);
+        },
 
-    return changeSlide(presentation, decrIndex(), previousIndex);
-}
+        load = function(id) {
+            //TODO : fetch with id
+            data = testData;
+            onContentChange(data[getIndex()], data.template);
+            onTemplateChange(date[getIndex()], data.template);
+        };
 
-function cleanSlides(slides) {
-    return slides.filter(function(slide) {
-        if (slide.text || slide.image) return slide;
-    });
-}
+    return {
+        updateContent : updateContent.bind(getIndex),
+        updateTemplate : updateTemplate,
+        load : load
+    }
+
+}());
+
+
+
+// SLIDES
 
 function createSlide() {
     return {
@@ -131,27 +127,61 @@ function createSlide() {
     }
 }
 
-function changeSlide(presentation, index, previousIndex) {
-    var cleanedSlides = cleanSlides(presentation.slides).length - presentation.slides.length;
+function changeSlide(presentation, index) {
 
-    if (cleanedSlides !== 0) {
-        index -= cleanedSlides;
-        previousIndex -= cleanSlides;
-        presentation.slides = cleanSlides(presentation.slides);
-    }
-
-    if (!presentation.slides[index]) {
-        presentation.slides = createSlide();
-    }
+    var slide = presentation.getSlide(index);
 
     //TODO : update max/current slide UI
 
     updateForms(presentation, index);
+    renderSlide(presentation.content[index], presentation.html);
+}
 
-    var slide = renderSlide(presentation.content[index], presentation.html);
+// PAGINATION
+
+var pagination = (function (onChange) {
+
+    var index = 0,
+
+        next = function() {
+
+            var previousIndex = index;
+            index++;
+            return onChange(index, previousIndex);
+        },
+
+        previous = function() {
+            if (index === 0)
+                return;
+
+            var previousIndex = index;
+
+            index--;
+            return onChange(index, previousIndex);
+        },
+
+        getIndex = function() {
+            return index;
+        };
+
+    return {
+        next : next,
+        previous : previous,
+        getIndex : getIndex
+    }
+
+}(changeSlide.bind(presentation)));
+
+// UI
+
+function renderSlide(slide, template) {
+    var div = document.createElement("div");
+
+    div.innerHTML = template.replace("text", slide.text)
+                            .replace("image", slide.image);
 
     document.querySelector(".slide").innerHTML = "";
-    document.querySelector(".slide").appendChild(slide);
+    document.querySelector(".slide").appendChild(div);
 }
 
 function updateStyle(css) {
@@ -164,24 +194,9 @@ function updateStyle(css) {
     style.textContent = css;
 }
 
-function updateContent(presentation, getIndex, content) {
-    presentation.content[getIndex()] = content;
-}
-
-function updateTemplate(presentation, template) {
-    presentation.html = template.html;
-    presentation.css = template.css;
-
-    updateStyle(template.css);
-}
-
-function renderSlide(content, template) {
-    var div = document.createElement("div");
-
-    if (content === undefined)
-        return div;
-
-    div.innerHTML = template.replace("text", content.text)
-                            .replace("image", content.image);
-    return div.firstChild;
+function updateForms(presentation, getIndex) {
+    document.querySelector(".content textarea").value = presentation.slides[getIndex()].text;
+    document.querySelector(".content [type=url]").value = presentation.slides[getIndex()].image;
+    document.querySelector(".template .html").value = presentation.html;
+    document.querySelector(".template .css").value = presentation.css;
 }
